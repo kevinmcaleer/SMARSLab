ddfaf3af02caeae3e5e1bd28c4ad886aefd82b94
Kevin McAleer <kevinmcaleer@me.com>
2019-03-30 15:17:45 +0000

    About to add bluetooth detection

--- command_history.py
+++ command_history.py
@@ -10,7 +10,7 @@ class CommandHistory(object):
         self.history.append("*** new history ***")
 
     def append(self, command):
-        """ adds a command to the command history """    
+        """ adds a command to the command history """
         self.history.append(command)
 
     def clear(self):
