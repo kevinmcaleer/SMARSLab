2c8448f93a41501516afb1a8e585c4f3de55f565
Kevin McAleer <kevinmcaleer@me.com>
2019-04-02 17:04:29 +0100

    updated the bluetooth utility app for testing bluetooth connectivity. Its a bit hacky as there is no universal python library for bluetooth.

--- butil.py
+++ butil.py
@@ -1,25 +1,46 @@
 import serial
 import time
+
 print("Start")
-port="/dev/tty.HC-05-DevB"
-bluetooth=serial.Serial(port, 9600)
+port="/dev/tty.HC-05-SPPDev"
+bluetooth=serial.Serial(port, 9600, timeout = 1)
+
 print("connected")
 delay = 0.5;
-bluetooth.flushInput()
-while True:
-    bluetooth.write(str.encode('u'))
-    print("up")
-    time.sleep(delay)
-    bluetooth.flushInput()
-    bluetooth.write(str.encode('d'))
-    print("down")
-    time.sleep(delay)
-    bluetooth.flushInput()
-    bluetooth.write(str.encode('l'))
-    print("left")
-    time.sleep(delay)
-    bluetooth.flushInput()
-    bluetooth.write(str.encode('r'))
-    print("right")
-    time.sleep(delay)
-    bluetooth.flushInput()
+
+nb = "1"
+while nb != "q":
+    # bluetooth.open()
+    nb = input('WASD: ')
+    if nb == "w":
+        bluetooth.write(b'u')
+        # bluetooth.write(str.encode()) # up
+    if nb == "s":
+        bluetooth.write(b'd') # down
+    if nb == "a":
+        bluetooth.write(b'l') # left
+    if nb == "d":
+        bluetooth.write(b'r') # right
+    # print (str.encode())
+    # bluetooth.flushInput()
+
+# while True:
+#     bluetooth.write(str.encode('u'))
+#     print("up")
+#     time.sleep(delay)
+#     bluetooth.flushInput()
+#
+#     bluetooth.write(str.encode('d'))
+#     print("down")
+#     time.sleep(delay)
+#     bluetooth.flushInput()
+#
+#     bluetooth.write(str.encode('l'))
+#     print("left")
+#     time.sleep(delay)
+#     bluetooth.flushInput()
+#
+#     bluetooth.write(str.encode('r'))
+#     print("right")
+#     time.sleep(delay)
+#     bluetooth.flushInput()
