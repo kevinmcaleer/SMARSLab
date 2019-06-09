a06ae4d100c60132600fde2ea3f3fae6d5cf7cf8
Kevin McAleer <kevinmcaleer@me.com>
2019-06-09 08:57:02 +0100

    Added instructions

--- create_venv.py
+++ create_venv.py
@@ -6,3 +6,8 @@ import subprocess
 
 print("Creating Python 3 Virtual Environment")
 subprocess.call(["virtualenv", "-p", "python3", "venv"])
+print("Environment created - To use the virtual environment, type:")
+print("  source venv/bin/activate")
+print("to activate the environment, and")
+print("  deactivate")
+print("to deactivate the environment.")
