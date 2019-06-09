18adf7a8dea6486ab5749bfed4848d1ed7db3291
Kevin McAleer <kevinmcaleer@me.com>
2019-06-09 07:47:10 +0100

    Added create venv script

---
+++ create_venv.py
@@ -0,0 +1,12 @@
+"""
+# create_venv.py
+Checks the os and creates a new virtual environment for SMARSLab
+
+"""
+
+import platform
+
+print("Platform: ", platform.platform())
+print("System: ", platform.system())
+print("Release: ", platform.release())
+print("Version: ", platform.version())
