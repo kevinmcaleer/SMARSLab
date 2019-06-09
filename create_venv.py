c3e56a001ab773101e742a1ccc0eb430be13d6fc
Kevin McAleer <kevinmcaleer@me.com>
2019-06-09 08:54:51 +0100

    fixed typo

--- create_venv.py
+++ create_venv.py
@@ -5,4 +5,4 @@ Creates a new virtual environment for SMARSLab
 import subprocess
 
 print("Creating Python 3 Virtual Environment")
-subprocess.call(["virtualenv", "-python3", "venv"])
+subprocess.call(["virtualenv", "-p", "python3", "venv"])
