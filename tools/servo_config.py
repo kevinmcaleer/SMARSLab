2da9885fef450f66cbb0b0642ab69b93842858c6
Kevin McAleer <kevinmcaleer@me.com>
2020-11-15 14:58:46 +0000

    Updated servo config

--- tools/servo_config.py
+++ tools/servo_config.py
@@ -2,10 +2,6 @@
 
 import smars_library.smars_library as sl
 
-quad = sl.SmarsRobot()
-
-quad.setname = "Quad"
 leg = sl.Leg(name= "front left leg", channel=0, leg_minangle=9, leg_maxangle=90, invert= True)
-quad
 print(f"Setting {leg.name} to default position")
 leg.default()
