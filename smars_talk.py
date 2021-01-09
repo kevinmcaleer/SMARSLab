4dd3bade123232f8ab29d739856020a2d82ffe46
Kevin McAleer <kevinmcaleer@gmail.com>
2021-01-09 16:00:01 +0000

    updated requirements to latest version of smars_library

--- smars_talk.py
+++ smars_talk.py
@@ -1,14 +1,12 @@
-# import pyttsx3
-import pyaudio
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
 
-p = pyaudio.PyAudio()
-info = p.get_host_api_info_by_index(0)
-numdevices = info.get('deviceCount')
-
-for i in range(0, numdevices):
-  if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-    print('Input Device ID:', i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
-
-# engine = pyttsx3.init()
-# engine.say("I will speak this text")
-# engine.runAndWait()
+if mic.wakeup('quad'):
+    print('wake up')
+    data = mic.listen()
+    text = mic.recognize(data)
+    if text:
+        time.sleep(1)
+        print('Recognized %s' % text)
