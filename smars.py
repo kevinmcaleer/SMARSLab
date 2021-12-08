218fb4e88a5ba89933c1e3ee27d0714ce2d6e708
Kevin McAleer <kevinmcaleer@gmail.com>
2021-12-08 23:04:44 +0000

    Create smars.py
    Initial Commit of v2 SMARS code

---
+++ smars.py
@@ -0,0 +1,26 @@
+""" 
+SMARSLab - SMARS code for the ESP32 v2
+Kevin McAleer December 2021
+"""
+
+# import the board connections
+from machine import Pin, UART
+
+class SMARS():
+  """ The SMARS Robot """
+  name = ""
+  
+  def __init__():
+    """ Initialise the Robot """
+    self.name = "SMARS"
+    pass
+  
+  def move_forward():
+    """ Move the Robot forward """
+    pass
+  
+  def move_backward():
+    """ Move the Robot backward """
+    pass
+  
+  
