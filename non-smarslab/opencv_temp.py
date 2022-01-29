87204fe668085991d0483da5d3aa1eb8dca66275
Kevin McAleer <kevinmcaleer@gmail.com>
2022-01-29 17:14:07 +0000

    It Works!

--- non-smarslab/opencv_temp.py
+++ non-smarslab/opencv_temp.py
@@ -2,6 +2,38 @@ import io
 import picamera
 import cv2
 import numpy
+import time
+from trilobot import *
+
+SHOW_TIME = 3
+CLEAR_TIME = 0.5
+
+tbot = Trilobot()
+
+def green_light():
+    tbot.fill_underlighting(0,0,0)
+    # time.sleep(CLEAR_TIME)
+    tbot.set_underlight(LIGHT_FRONT_LEFT, 0,255,0, show=False)
+    tbot.set_underlight(LIGHT_MIDDLE_LEFT, 0,255,0, show=False)
+    tbot.set_underlight(LIGHT_REAR_LEFT, 0,255,0, show=False)
+    tbot.set_underlight(LIGHT_FRONT_RIGHT, 0,255,0, show=False)
+    tbot.set_underlight(LIGHT_MIDDLE_RIGHT, 0,255,0, show=False)
+    tbot.set_underlight(LIGHT_REAR_RIGHT, 0,255,0, show=False)
+    tbot.show_underlighting()
+
+def blue_light():
+    tbot.fill_underlighting(0,0,0)
+    # time.sleep(CLEAR_TIME)
+    tbot.set_underlight(LIGHT_FRONT_LEFT, 0,0,255, show=False)
+    tbot.set_underlight(LIGHT_MIDDLE_LEFT, 0,0,255, show=False)
+    tbot.set_underlight(LIGHT_REAR_LEFT, 0,0,255, show=False)
+    tbot.set_underlight(LIGHT_FRONT_RIGHT, 0,0,255, show=False)
+    tbot.set_underlight(LIGHT_MIDDLE_RIGHT, 0,0,255, show=False)
+    tbot.set_underlight(LIGHT_REAR_RIGHT, 0,0,255, show=False)
+    tbot.show_underlighting()
+    # time.sleep(SHO)
+
+
 
 def detect_faces():
     stream = io.BytesIO()
@@ -22,11 +54,16 @@ def detect_faces():
     faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
     print("Found " + str(len(faces)) + " face(s)")
+    if len(faces) > 0:
+        green_light()
+    else:
+        blue_light()
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(image, (x,y), (x+w,y+h),(255,255,0),4)
+    # write file
+    # for (x,y,w,h) in faces:
+    #     cv2.rectangle(image, (x,y), (x+w,y+h),(255,255,0),4)
 
-    cv2.imwrite('result.jpg', image)
+    # cv2.imwrite('result.jpg', image)
 
 while True or KeyboardInterrupt:
     detect_faces()
